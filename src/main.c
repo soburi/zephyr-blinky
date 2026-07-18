af972a8e6644f0f74dabccfd0cb509d72c521c66
TOKITA Hiroshi <tokita.hiroshi@gmail.com>
2026-07-18 18:37:03 +0900

    gpio2-check

--- src/main.c
+++ src/main.c
@@ -11,33 +11,47 @@
 /* 1000 msec = 1 sec */
 #define SLEEP_TIME_MS   1000
 
-/* The devicetree node identifier for the "led0" alias. */
+/* The devicetree node identifiers for the GPIO2 LED aliases. */
 #define LED0_NODE DT_ALIAS(led0)
+#define LED1_NODE DT_ALIAS(led1)
+#define LED2_NODE DT_ALIAS(led2)
+#define LED3_NODE DT_ALIAS(led3)
+#define LED4_NODE DT_ALIAS(led4)
 
 /*
  * A build error on this line means your board is unsupported.
  * See the sample documentation for information on how to fix this.
  */
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec leds[] = {
+	GPIO_DT_SPEC_GET(LED0_NODE, gpios),
+	GPIO_DT_SPEC_GET(LED1_NODE, gpios),
+	GPIO_DT_SPEC_GET(LED2_NODE, gpios),
+	GPIO_DT_SPEC_GET(LED3_NODE, gpios),
+	GPIO_DT_SPEC_GET(LED4_NODE, gpios),
+};
 
 int main(void)
 {
 	int ret;
 	bool led_state = true;
 
-	if (!gpio_is_ready_dt(&led)) {
-		return 0;
-	}
+	for (size_t i = 0; i < ARRAY_SIZE(leds); i++) {
+		if (!gpio_is_ready_dt(&leds[i])) {
+			return 0;
+		}
 
-	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-	if (ret < 0) {
-		return 0;
+		ret = gpio_pin_configure_dt(&leds[i], GPIO_OUTPUT_ACTIVE);
+		if (ret < 0) {
+			return 0;
+		}
 	}
 
 	while (1) {
-		ret = gpio_pin_toggle_dt(&led);
-		if (ret < 0) {
-			return 0;
+		for (size_t i = 0; i < ARRAY_SIZE(leds); i++) {
+			ret = gpio_pin_toggle_dt(&leds[i]);
+			if (ret < 0) {
+				return 0;
+			}
 		}
 
 		led_state = !led_state;
