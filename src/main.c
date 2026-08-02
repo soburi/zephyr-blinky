4568be36279bebb80c91dbe5f2ea805a8abf5fc3
TOKITA Hiroshi <tokita.hiroshi@gmail.com>
2026-08-02 21:22:10 +0900

    allpins

--- src/main.c
+++ src/main.c
@@ -13,12 +13,20 @@
 
 /* The devicetree node identifier for the "led0" alias. */
 #define LED0_NODE DT_ALIAS(led0)
+#define LED1_NODE DT_ALIAS(led1)
+#define LED2_NODE DT_ALIAS(led2)
+#define LED3_NODE DT_ALIAS(led3)
+#define LED4_NODE DT_ALIAS(led4)
 
 /*
  * A build error on this line means your board is unsupported.
  * See the sample documentation for information on how to fix this.
  */
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
+static const struct gpio_dt_spec led2 = GPIO_DT_SPEC_GET(LED2_NODE, gpios);
+static const struct gpio_dt_spec led3 = GPIO_DT_SPEC_GET(LED3_NODE, gpios);
+static const struct gpio_dt_spec led4 = GPIO_DT_SPEC_GET(LED4_NODE, gpios);
 
 int main(void)
 {
@@ -28,14 +36,66 @@ int main(void)
 	if (!gpio_is_ready_dt(&led)) {
 		return 0;
 	}
+	if (!gpio_is_ready_dt(&led1)) {
+		return 0;
+	}
+	if (!gpio_is_ready_dt(&led2)) {
+		return 0;
+	}
+	if (!gpio_is_ready_dt(&led3)) {
+		return 0;
+	}
+	if (!gpio_is_ready_dt(&led4)) {
+		return 0;
+	}
 
 	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
 	if (ret < 0) {
 		return 0;
 	}
 
+	ret = gpio_pin_configure_dt(&led1, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+		return 0;
+	}
+
+	ret = gpio_pin_configure_dt(&led2, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+		return 0;
+	}
+
+	ret = gpio_pin_configure_dt(&led3, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+		return 0;
+	}
+
+	ret = gpio_pin_configure_dt(&led4, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+		return 0;
+	}
+
 	while (1) {
 		ret = gpio_pin_toggle_dt(&led);
+		if (ret < 0) {
+			return 0;
+		}
+
+		ret = gpio_pin_toggle_dt(&led1);
+		if (ret < 0) {
+			return 0;
+		}
+
+		ret = gpio_pin_toggle_dt(&led2);
+		if (ret < 0) {
+			return 0;
+		}
+
+		ret = gpio_pin_toggle_dt(&led3);
+		if (ret < 0) {
+			return 0;
+		}
+
+		ret = gpio_pin_toggle_dt(&led4);
 		if (ret < 0) {
 			return 0;
 		}
